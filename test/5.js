343741b3806f7bc135b39a493f7c801d2154d8b6
av8ta <av8ta@protonmail.com>
2021-06-03 15:40:34 +0800

    hook before and after tests with node scripts

--- test/5.js
+++ test/5.js
@@ -1,8 +1,6 @@
 const test = require('ava')
 const createSsbServer = require('../')
 
-require('./utils/setup-test')
-
 test.serial('5. creates stack and loads user plugins', t => {
   const sbot = createSsbServer({ appname: 'ssb-shipyard' })
   t.true(sbot.config.appname === 'ssb-shipyard')
--- test/utils/setup-test.js
+++ test/utils/setup-test.js
@@ -5,10 +5,6 @@ const fse = require('fs-extra')
 
 const path = join(home, '.ssb-shipyard')
 rimraf.sync(path)
-// rimraf.sync(join(home, '.ssb-shipyard-test'))
-// rimraf.sync(join(home, '.ssb-shipyard-test2'))
-// rimraf.sync(join(home, '.ssb-shipyard-test3'))
-// rimraf.sync(join(home, '.ssb-shipyard-test4'))
 
 const src = '/home/av8ta/metacentre/shipyard/test/ssb-shipyard'
 
